8e528398eccfc459878e3d5ab04239a7fe246e9c
Khushwant <khushwantmanglani6@gmail.com>
2024-05-17 11:08:30 +0530

    Update: Move global error handling to the end

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { errorHandling } from "./middlewares/errorHandling.middleware.js";
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
-app.use(errorHandling);
 
 // routes import
 import customerRouter from "./routes/customer.routes.js";
 
 // routes declaration
 app.use("/api/v1/customer", customerRouter);
+
+// global error handler
+app.use(errorHandling);
 
 export { app };
